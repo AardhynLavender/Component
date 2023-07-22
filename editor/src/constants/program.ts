2bd991c625191b8ccbfa1c39362d7e59eac5b7e5
Aardhynlavneder <laveat1@student.op.ac.nz>
2023-07-22 21:51:19 +1200

    fix(types): comfort typescript

--- editor/src/constants/program.ts
+++ editor/src/constants/program.ts
@@ -1,8 +1,10 @@
+import { Program } from 'structures/program';
+
 export const LOCAL_STORAGE_KEY = 'componentProgram';
 
 export const AST_VERSION = '0.0.1';
 
-export const BLANK_PROGRAM = {
+export const BLANK_PROGRAM: Program = {
   version: AST_VERSION,
   name: 'Untitled Program',
   ast: [
