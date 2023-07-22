2b4b5676b825f9b8392d5a5f35e657b87efcf207
Aardhynlavneder <laveat1@student.op.ac.nz>
2023-07-22 21:17:11 +1200

    chore: uniform padding

--- editor/src/routes/Header.tsx
+++ editor/src/routes/Header.tsx
@@ -2,6 +2,7 @@ import { CoreApi } from '../types/api';
 import useComponentStore from 'structures/program/store';
 import { s, CSS, styled } from 'theme/stitches.config';
 import Button from 'ui/Button';
+import { H1, H3 } from 'theme/Typography';
 
 export default function Header({ core, css }: { core: CoreApi; css: CSS }) {
   const program = useComponentStore((state) => state.program);
@@ -11,13 +12,14 @@ export default function Header({ core, css }: { core: CoreApi; css: CSS }) {
 
   return (
     <Root css={css}>
+      <H3>Component</H3>
       <Button onClick={() => handleRun(core)}>Run</Button>
     </Root>
   );
 }
 
 const Root = styled('section', {
-  p: 16,
+  p: 8,
   d: 'flex',
   gap: 16,
   bb: '2px solid $outline',
