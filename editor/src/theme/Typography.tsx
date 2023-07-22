15b2d7cdce1429e3542496e8c426978115bae3e3
Aardhynlavneder <laveat1@student.op.ac.nz>
2023-07-22 15:24:00 +1200

    feat: typography components

---
+++ editor/src/theme/Typography.tsx
@@ -0,0 +1,72 @@
+import { styled } from './stitches.config';
+
+const standardVariants = {
+  variants: {
+    color: {
+      primary: { color: '$primary' },
+      tonal1: { color: '$tonal1' },
+      tonal2: { color: '$tonal2' },
+    },
+  },
+};
+
+export const H1 = styled('h1', {
+  color: '$text',
+  fontSize: 48,
+  fontWeight: '$bold',
+  lineHeight: 1.2,
+
+  ...standardVariants,
+});
+
+export const H2 = styled('h2', {
+  color: '$text',
+  fontSize: 32,
+  fontWeight: '$bold',
+  lineHeight: 1.2,
+
+  ...standardVariants,
+});
+
+export const H3 = styled('h3', {
+  color: '$text',
+  fontSize: 24,
+  fontWeight: '$bold',
+  lineHeight: 1.2,
+
+  ...standardVariants,
+});
+
+export const H4 = styled('h4', {
+  color: '$text',
+  fontSize: 20,
+  fontWeight: '$bold',
+  lineHeight: 1.2,
+
+  ...standardVariants,
+});
+
+export const H5 = styled('h5', {
+  color: '$text',
+  fontSize: 16,
+  fontWeight: '$bold',
+  lineHeight: 1.2,
+
+  ...standardVariants,
+});
+
+export const Span = styled('span', {
+  color: '$text',
+  fontSize: 16,
+  fontWeight: '$normal',
+  lineHeight: 1.2,
+
+  ...standardVariants,
+});
+
+export const A = styled('a', {
+  all: 'unset',
+  color: '$primary',
+
+  ...standardVariants,
+});
