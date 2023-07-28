a6e2181c3c8b5d4d8206778a91c3ee0633b7b77b
Aardhyn Lavender <83677410+AardhynLavender@users.noreply.github.com>
2023-07-28 15:48:02 +1200

    Delete Timing.hpp

--- core/include/Timing.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <chrono>
-
-namespace Timing {
-    using namespace std::chrono;
-    inline system_clock::time_point Now() { return system_clock::now(); }
-    inline milliseconds Elapsed(system_clock::time_point since) { return duration_cast<milliseconds>(Now() - since); }
-    inline bool Elapsed(system_clock::time_point since, milliseconds amount) { return Elapsed(since) > amount; }
-}
