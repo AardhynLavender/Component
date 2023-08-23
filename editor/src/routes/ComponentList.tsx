1588cf9eed93e44c515a1e7a215f48c19565052a
Aardhynlavneder <laveat1@student.op.ac.nz>
2023-08-23 22:52:23 +1200

    fix: bad import

--- editor/src/routes/ComponentList.tsx
+++ editor/src/routes/ComponentList.tsx
@@ -14,6 +14,7 @@ import { Capitalize } from 'util/string';
 import { uuid } from 'util/uuid';
 import { useMemo } from 'react';
 import Scroll from 'ui/Scroll';
+import ErrorBoundary from 'exception/ErrorBoundary';
 
 const ComponentCategories = {
   blocks: blockTypes,
