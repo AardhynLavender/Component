85cb209f655cc9c322d3c9d31f383c2da9926d50
Aardhynlavneder <laveat1@student.op.ac.nz>
2023-10-16 21:54:12 +1300

    feat: component list hiding blacklist

--- editor/src/routes/ComponentList.tsx
+++ editor/src/routes/ComponentList.tsx
@@ -22,6 +22,7 @@ import { Drag } from '../util/Drag';
 import { renderers } from 'program/components/types';
 import Spacer from 'components/util/Spacer';
 
+const SPACER = '';
 const ComponentCategories = {
   misc: ['comment'],
   output: outputs,
@@ -29,23 +30,28 @@ const ComponentCategories = {
   rendering: renderers,
   operators: [
     ...binaryOperators,
-    '',
+    SPACER,
     ...unaryOperators,
-    '',
+    SPACER,
     'increment',
     'decrement',
   ],
-  conditions: ['branch', ...conditions],
-  lists: ['list', 'subscript', 'append', 'size'],
+  conditions: ['branch', SPACER, ...conditions],
+  lists: ['list', SPACER, 'subscript', 'append', 'size'],
   variables,
 } as const;
 type ComponentCategory = keyof typeof ComponentCategories;
+
+const hiddenComponents: readonly ComponentType[] = ['size'];
+function isHiddenComponent(type: ComponentType) {
+  return !hiddenComponents.includes(type);
+}
 
 export default function ComponentList() {
   // so... when you copy a component out of the list, I need the uuid's to refresh
   // and the simplest is to force <ComponentList /> to re-render when `program` changes.
   // Achieved by reducing program state from the component store so zustand creates a
-  // signal. This is a bit of a  hack, but it'll do for now.
+  // signal for this component. This is a bit of a hack... but it'll do for now.
   const _ = useComponentStore((store) => store.program);
 
   const { Dropzone: DeleteDropzone } = Drag.useRemoveComponentOnDrop();
@@ -76,24 +82,21 @@ function ComponentListCategory({
   category: keyof typeof ComponentCategories;
   components: readonly ComponentType[];
 }) {
+  const isVariableCategory = category === 'variables';
+
   return (
     <CategoryRoot>
       <CategoryName>{Capitalize(category)}</CategoryName>
       <ComponentListRoot>
-        {components.map((type, index) => {
-          if (!type) return <Spacer key={index} height="sm" />;
+        {components.filter(isHiddenComponent).map((type, index) => {
+          if (!type) return <Spacer key={index} height="ty" />;
           const component = CreateComponent(type);
           const parent = undefined;
           const preview = true;
           return GetJsxComponent(component, parent, preview);
         })}
       </ComponentListRoot>
-      {category === 'variables' && (
-        <>
-          <Spacer height="sm" />
-          <VariableStoreList />
-        </>
-      )}
+      {isVariableCategory && <VariableStoreList />}
     </CategoryRoot>
   );
 }
@@ -113,19 +116,19 @@ const ComponentListRoot = styled('div', {
   fd: 'column',
   alignItems: 'flex-start',
   gap: 8,
-  p: 8,
+  px: 8,
+  py: 16,
 });
 
 function VariableStoreList() {
   const { variables } = useVariableStore();
 
   const keys = Object.keys(variables);
+  const noKeys = !keys.length;
 
   return (
     <ComponentListRoot>
-      {!keys.length && (
-        <s.span css={{ c: '$text2' }}>You have no variables</s.span>
-      )}
+      {noKeys && <s.span css={{ c: '$text2' }}>You have no variables</s.span>}
       {keys.map((definitionId) => (
         <VariableStoreItem key={definitionId} definitionId={definitionId} />
       ))}
