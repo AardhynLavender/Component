b0ee05fedbdcdf1b8ba736c78ecf053086e0472c
Aardhynlavneder <laveat1@student.op.ac.nz>
2023-10-16 21:52:21 +1300

    chore: tiny spacer

--- editor/src/components/util/Spacer.tsx
+++ editor/src/components/util/Spacer.tsx
@@ -3,12 +3,14 @@ import { styled, s } from 'theme/stitches.config';
 const Spacer = styled(s.div, {
   variants: {
     width: {
+      ty: { w: 12 },
       sm: { w: 16 },
       md: { w: 64 },
       lg: { w: 128 },
       xl: { w: 512 },
     },
     height: {
+      ty: { h: 12 },
       sm: { h: 16 },
       md: { h: 64 },
       lg: { h: 128 },
