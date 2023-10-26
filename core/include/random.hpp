4c34951c2cb282560ee781e4e8f5941eeca344ce
Aardhynlavneder <laveat1@student.op.ac.nz>
2023-10-27 11:11:02 +1300

    fix(random): divide by zero exception

--- core/include/random.hpp
+++ core/include/random.hpp
@@ -3,10 +3,17 @@
 template <typename T>
 concept Numeric = std::is_arithmetic_v<T>;
 
-template<Numeric T>
+template<Numeric T = int>
 T GenerateRandomNumber(T min, T max) {
   std::random_device device;
   std::mt19937 mte{device()};
-  std::uniform_real_distribution<T> distribution(min, max);
-  return distribution(mte);
+  if constexpr (std::is_integral_v<T>) {
+    std::uniform_int_distribution<T> distribution(min, max);
+    return distribution(mte);
+  } 
+  if constexpr (std::is_floating_point_v<T>) {
+    std::uniform_real_distribution<T> distribution(min, max);
+    return distribution(mte); 
+  }
+  throw std::invalid_argument("Invalid numeric type provided!");
 }
